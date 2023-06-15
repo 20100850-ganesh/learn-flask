293384c3a1eb92d53a7aff74ed05cc06c9612863
20100850-ganesh <20100850@mail.wit.ie>
2023-06-15 20:27:52 +0530

    Templates,Styles and layout added

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'dummy1',
+        'title': 'Blog Post 1',
+        'content': 'This is a first blog post content posted by dummy 1 for the flask web app.',
+        'date_posted': 'June 15, 2023'
+    },
+    {
+        'author': 'dummy2',
+        'title': 'Blog Post 2',
+        'content': 'This is another blog post content posted by dummy 2 for the flask web app.',
+        'date_posted': 'June 16, 2023'
+    }
+]
+
 
 @app.route('/')
-def hello():
-    return '''
-      <h1>Hello, Flask!</h1>
-      <p>This is a Simple Flask Application built using Python</p>
-  '''
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
 
 
 if __name__ == "__main__":
